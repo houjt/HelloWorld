e13479b45095cb78b7f92c408939a97db887fd6a
houjt <2644484013@qq.com>
2016-05-22 22:23:24 +0800

    CustomView

---
+++ app/src/main/java/com/example/hou/myapplication/CustomView.java
@@ -0,0 +1,22 @@
+package com.example.hou.myapplication;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by hou on 2016/5/22.
+ */
+public class CustomView extends View{
+    public CustomView(Context context) {
+        super(context);
+    }
+
+    public CustomView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public CustomView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
